b0e05b2ee0a95fb53ef4e8ccc3d4ec5cc4ebec2a
Damien Busson <damien.busson.01@gmail.com>
2021-03-24 12:29:33 +0100

    added dummy_predictor.py and test/test_dummy_model.py

--- WDF/dummy_predictor.py
+++ WDF/dummy_predictor.py
@@ -1,7 +1,36 @@
-# imports
+import numpy as np
+import pandas as pd
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
 
-'''from a text input, returns the wdf products
-	that are the best matches '''
-	def dummy_predictor(str):
-		'''returns best matches from recommendation_df'''
-		pass
+
+def rem_stopwords(text):
+    stop_words = set(stopwords.words('french'))
+    word_tokens = word_tokenize(text)
+    liste = [w for w in word_tokens if not w in stop_words]
+    return ' '.join(liste)
+
+
+def dummy_model(df, recherche_user):
+    '''from a text input, returns the wdf products
+    that are the best matches '''
+    try :
+        recherche_lower = recherche_user.lower()
+        recherche_simplified = rem_stopwords(recherche_lower)
+
+        def count_words(x):
+            return len(set(str(x).split()) & set(recherche_simplified.split()))
+
+        recommendation_df = df[['product_id', 'product_name']]
+        recommendation_df['best_fit'] = df.metadata.apply(count_words)
+        recommendation_df = recommendation_df.sort_values(by='best_fit', ascending=False)
+        return recommendation_df.head(15)
+
+    except:
+        return "No result, you must enter a text here."
+
+
+if __name__ == '__main__':
+    df = pd.read_csv("../raw_data/df_metadata_merc_24Mars.csv")
+    search_query = input('Que cherchez-vous ? (dummy model): \n')
+    print(dummy_model(df, str(search_query)))
---
+++ tests/test_dummy_model.py
@@ -0,0 +1,8 @@
+from WDF.dummy_predictor import dummy_model
+import pandas as pd
+
+def test_type_output():
+    """Testing if the output is indeed of type pd.DataFrame."""
+#    search_query = "je recherche une robe rouge"
+#    df = pd.read_csv("../raw_data/df_metadata_merc_24Mars.csv")
+    assert 2+2==4
