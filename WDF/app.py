792161063caf4a0c3ea1d318632018574c4fd982
magloire ndabagera <magloirendabagera@gmail.com>
2021-03-25 17:32:39 +0100

    changing dummy preditcor, improving streamlit app and adding empty function for data.py

--- WDF/app.py
+++ WDF/app.py
@@ -13,16 +13,26 @@ def icon(icon_name):
     st.markdown(f'<i class="material-icons">{icon_name}</i>', unsafe_allow_html=True)
 
 
-local_css("./css/style.css")
+##############
+## CSS #####
+##############
+
+local_css("WDF/css/style.css")
 remote_css('https://fonts.googleapis.com/icon?family=Material+Icons')
 
-icon("memory")
-df = pd.read_csv("../raw_data/df_metadata_merc_24Mars.csv")
+df = pd.read_csv("./raw_data/mag_test.csv")
+st.image('WDF/css/assets/logo.svg')
 search_query = st.text_input("", "un jean bleu")
 df_res = dummy_model(df, str(search_query))
+df_res.drop_duplicates(subset='product_id',inplace=True)
 button_clicked = st.button("OK")
+
+
+num =len(df_res)
 if st.checkbox('Show result', False):
-    st.write(df_res)
+    for i in range(len(df_res)):
+        st.image(df_res.iloc[i]['photos'],width=300,
+                        caption=df_res.iloc[i]['product_name'])
 
 
 # if __name__ == '__main__':
